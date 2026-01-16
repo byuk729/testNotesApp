ac53b459f6080ccb9d74b97288052a8a38b0b06c
byuk729 <108688462+byuk729@users.noreply.github.com>
2026-01-16 00:08:13 -0500

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+
 let notes = [
   {
     id: "1",
@@ -21,9 +22,6 @@ let notes = [
     important: true
   }
 ]
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
